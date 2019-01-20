c25faa1665658f7630593618d6ecd1ffa0e09c54
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-20 14:58:01 -0500

    JuliaRanMoreThanPranav

---
+++ JuliaRanMoreThanPranav/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JuliaRanMoreThanPranav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Last week Julia ran 30 miles more than Pranav. Julia ran 47 miles. How many miles did Pranav run?
+
+            int juliaRan = 47;
+            int juliaRanMore = 30;
+            int pranavRan = juliaRan - juliaRanMore;
+            Console.WriteLine("Pranav ran " + pranavRan + " miles.");
+        }
+    }
+}
